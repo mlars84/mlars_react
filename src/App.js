986dc48cb3a4be7248b21e7708347853cc17b411
Matt Larson <matt.a.larson@gmail.com>
2017-07-31 15:03:04 -0500

    setting up props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1><em>Hello folks! React is super fun!</em></h1>
+    let txt = this.props.txt;
+    return (
+      <div>
+        <h1><em>{txt}</em></h1>
+        <h2>Some more cool text!</h2>
+      </div>
+    )
   }
 };
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "This is the default text."
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App cat={5} txt="this is the prop value"/>,
+  document.getElementById('root')
+);
