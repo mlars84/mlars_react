0922e3b634209c356bb9e1f9688a4bf52526b838
Matt Larson <matt.a.larson@gmail.com>
2017-07-31 14:07:56 -0500

    stripped down to barebones React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>mlars-React-app</h2>
-        </div>
-        <div className="App-menu">
-          <nav>
-            <a href="/home">Home</a>
-            <a href="/work">Work</a>
-            <a href="/about">About</a>
-          </nav>
-        </div>
-        <p className="App-intro">
-          This is my first app built with ReactJS <button>Button</button>
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <h1>Hello</h1>
 
-export default App;
+export default App 
