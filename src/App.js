77102555be5d1dd9eaab940d6e990bfcb1183341
Matt Larson <matt.a.larson@gmail.com>
2017-07-31 16:30:18 -0500

    textarea

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
+import css from './App';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this)
+  }
+  update( e ){
+    this.setState({ currentEvent: e.type })
+  }
   render(){
-    let txt = this.props.txt;
     return (
       <div>
-        <h1><em>{txt}</em></h1>
-        <h2>Some more cool text!</h2>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          onTouchStart={this.update}
+          onTouchMove={this.update}
+          onTouchEnd={this.update}
+          cols="30"
+          rows="10"/>
+          <h1>{this.state.currentEvent}</h1>
       </div>
     )
   }
-};
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
 }
 
-App.defaultProps = {
-  txt: "This is the default text."
-}
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       txt: 'this is the state text'
+//     }
+//   }
+//   update( e ){
+//     this.setState({ txt: e.target.value });
+//   }
+//   render(){
+//     let txt = this.props.txt;
+//     return (
+//       <div>
+//         <textarea
+//           cols="30"
+//           rows="10"/>
+//         <h1>{this.state.currentEvent}</h1>
+//       </div>
+//       <div>
+//         <Button>I <Heart /> React</Button>
+//         <h2>{this.state.txt}</h2>
+//         <Widget update={this.update.bind(this)}/>
+//         <Widget update={this.update.bind(this)}/>
+//         <Widget update={this.update.bind(this)}/>
+//         <h1><em>{txt}</em></h1>
+//         <h2>Some more cool text!</h2>
+//       </div>
+//     )
+//   }
+// };
+
+// const Button = (props) => <button>{props.children}</button>
+//
+// class Heart extends React.Component {
+//   render() {
+//     return <span>&hearts;</span>
+//   }
+// }
+//
+//
+// const Widget = (props) =>
+// <input type="text" onChange={props.update}/>
+//
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: 'This is the default text.'
+// }
 
 export default App;
